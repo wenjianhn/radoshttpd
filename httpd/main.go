0c9859ab4c8d65115d08d26ff0fd09c618fd9e11
Dongmao Zhang <zhangdongmao@letv.com>
2014-10-30 17:36:04 +0800

    http server nearly done

--- httpd/main.go
+++ httpd/main.go
@@ -1,11 +1,15 @@
 package main
 import (
     "github.com/thesues/radoshttpd/rados"
+    "github.com/codegangsta/martini"
     "fmt"
     "errors"
     "os"
+    "net/http"
+    "io"
 )
 
+/* this getData function is to test download remote rados file */
 func getData(striper rados.StriperPool, oid string, filename string) error{
     var offset int = 0
     var count  int = 0
@@ -34,6 +38,22 @@ func getData(striper rados.StriperPool, oid string, filename string) error{
    return nil
 }
 
+type RadosDownloader struct {
+  striper *rados.StriperPool
+  soid string
+  offset uint64
+}
+
+func (rd *RadosDownloader) Read(p []byte) (n int, err error) {
+  count, err := rd.striper.Read(rd.soid, p, uint64(rd.offset))
+  if count == 0 {
+    return 0, io.EOF
+  }
+  rd.offset += uint64(count)
+  return count, err
+}
+
+
 func main() {
     conn, err := rados.NewConn("admin")
     if err != nil {
@@ -47,22 +67,57 @@ func main() {
     }
     defer conn.Shutdown()
 
+    /* start from here */
+    /*
     pool, err := conn.OpenPool("data")
     if err != nil {
         return
     }
     defer pool.Destroy()
 
-
-
     striper, err := pool.CreateStriper()
     if err != nil {
         return
     }
     defer striper.Destroy()
+    */
 
+    /*
     err= getData(striper, "go", "dat.0")
     if err != nil {
         fmt.Println("error")
     }
+    */
+
+    m := martini.Classic()
+     m.Get("/(?P<pool>[A-Za-z0-9]+)/(?P<soid>[A-Za-z0-9]+)", func(params martini.Params, w http.ResponseWriter, r *http.Request){
+         poolname := params["pool"]
+         soid := params["soid"]
+         pool, err := conn.OpenPool(poolname)
+         if err != nil {
+           /* FIXME */
+           fmt.Println("open pool failed")
+           return
+         }
+         defer pool.Destroy()
+
+         striper, err := pool.CreateStriper()
+         if err != nil {
+           fmt.Println("open pool failed")
+           return
+         }
+         defer striper.Destroy()
+
+
+         rd := RadosDownloader{&striper, soid, 0}
+         /* set content-type */
+         w.Header().Set("Content-Disposition", "attachment; filename=Wiki.png")
+         w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+
+         /* set the stream */
+         io.Copy(w,&rd)
+     })
+
+    /* end */
+    http.ListenAndServe(":3000", m)
 }
